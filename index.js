5c04ed3df98af83ff42ff15f433694bc54ae9537
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-06-21 22:08:24 -0300

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-const express = require('express');
+const express = require("express");
+const cors = require('cors');
+
 const port = 3000;
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.use(express.json()); //para a aplicação utilizar o express.json, transforma tds as req e res em json para transitar mais leve no front/back
+app.use(cors());//ativar o cors
+
+const tarefas = [
+  {
+    id: 1,
+    tarefa: "Escovar os dentes",
+    descricao: "Acordar, pegar a pasta, colocar na escova e escovar os dentes",
+  },
+  {
+    id: 2,
+    tarefa: "Tomar Café",
+    descricao: "Pegar o café na cafeteira e colocar no copo, adoçar e beber",
+  },
+  {
+    id: 3,
+    tarefa: "Ligar o carro",
+    descricao: "Pegar a chave, colocar na ignição e dar o giro",
+  },
+];
+/*rota getAll*/
+
+app.get("/tarefas/todas-tarefas", (req, res) => {
+  res.send(tarefas);
 });
 
+/*rota getById*/
+
+app.get('/tarefas/tarefa/:id', (req, res) => {
+    const parametroId = Number(req.params.id);
+    const escolhaTarefa = tarefas.find((tarefa) => tarefa.id === parametroId);
+    res.send(escolhaTarefa);
+  });
+
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
